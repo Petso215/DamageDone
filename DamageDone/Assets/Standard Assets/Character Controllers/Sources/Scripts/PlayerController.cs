70e449f5f73f8ae54da2193c3732d58ed7681d1a
Petso215 <petso__@hotmail.com>
2014-11-06 23:01:01 +0000

    First Upload
    Character and Bed prop

---
+++ DamageDone/Assets/Standard Assets/Character Controllers/Sources/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{	
+	public float speed = 6f;
+	public float left = 0;
+	public float right = 0;
+	Animator anim;
+
+
+
+	
+	// START - Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// UPDATE is called once per frame
+	void Update () 
+		
+
+
+	{
+
+
+		// Moves player left, right, up, down. No collision.
+
+		float x = Input.GetAxis("Horizontal") * Time.smoothDeltaTime * speed;
+		float y = Input.GetAxis("Vertical") * Time.smoothDeltaTime * speed;
+		anim.SetFloat("speed", Input.GetAxis ("Horizontal"));
+
+		/*if (Input.GetAxis ("Horizontal") < 0) {
+			//transform.Translate(x,0,y,Space.Self);
+			//transform.eulerAngles = new Vector2(0, 0);
+			anim.SetFloat ("left", Mathf.Abs (Input.GetAxis ("Horizontal")));
+		}
+		if (Input.GetAxis ("Horizontal") > 0) {
+			//transform.Translate(x,0,y,Space.Self);
+			//transform.eulerAngles = new Vector2(0, 180);
+			anim.SetFloat ("right", Mathf.Abs (Input.GetAxis ("Horizontal")));
+		}
+		if (Input.GetAxis ("Horizontal") == 0) {
+			//transform.Translate(x,0,y,Space.Self);
+			//transform.eulerAngles = new Vector2(0, 180);
+			anim.SetFloat ("Idol", Mathf.Abs (Input.GetAxis ("Horizontal")));
+		}*/
+
+		transform.Translate(x,0,y,Space.Self);
+	}
+}
